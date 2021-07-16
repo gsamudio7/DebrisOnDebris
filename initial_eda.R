1a899999cce16fd371c87227952c6cc2d8a79a58
developer <developer@developers-MBP.fios-router.home>
2021-07-16 14:13:16 -0400

    update

--- initial_eda.R
+++ initial_eda.R
@@ -4,16 +4,27 @@ library(data.table)
 library(dplyr)
 
 
-# Read in the data
-observations <- fread("DebrisOnDebris.csv", fill=TRUE)
+## Read in the data (executed before initial clean)
+# observations <- fread("DebrisOnDebris.csv", fill=TRUE)
 
 
-# Transform data to only the relavant columns (based on Mr. Hejduk's guidance)
-observations <- observations[,c(48,49,9,6,12,14,15)]
-observations <- observations %>% 
-  setnames(c("V48","V49","V9","V6","V12","V14","V15"),
-           c("Event Number","Days to TCA","Pc Best","Pc Nom","ProbCatIfColl",
-             "NumFragIfCatColl","NumFragIfNonCatColl"))
+# # Transform data to only the relevant columns (based on Mr. Hejduk's guidance)
+# observations <- observations[,c(48,49,9,6,12,14,15)]
+# observations <- observations %>% 
+#   setnames(c("V48","V49","V9","V6","V12","V14","V15"),
+#            c("Event Number","Days to TCA","Pc Best","Pc Nom","ProbCatIfColl",
+#              "NumFragIfCatColl","NumFragIfNonCatColl"))
+# 
+# # Write
+# fwrite(observations,"trimmedData.csv")
 
-# Write
-fwrite(observations,"trimmedData.csv")
+
+# Read in trimmed data
+observations <- fread("trimmedData.csv")
+
+# Insert PcNom value when there is no PcBest
+observations[,"Pc" := ifelse(is.na(`Pc Best`),`Pc Nom`,`Pc Best`)]
+
+# Drop redundant Pc columns so we have only one Pc column
+observations <- observations[,!c("Pc Best","Pc Nom")]
+
