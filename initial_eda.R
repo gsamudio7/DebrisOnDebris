f9b32926ac27e2ced0bb15e86305a1180ab7c884
developer <developer@developers-MBP.fios-router.home>
2021-07-16 13:04:27 -0400

    fixed trimmedData

--- initial_eda.R
+++ initial_eda.R
@@ -9,8 +9,7 @@ observations <- fread("DebrisOnDebris.csv", fill=TRUE)
 
 
 # Transform data to only the relavant columns (based on Mr. Hejduk's guidance)
-
-observations <- observations[,c(49,48,9,6,12,14,15)]
+observations <- observations[,c(48,49,9,6,12,14,15)]
 observations <- observations %>% 
   setnames(c("V49","V48","V9","V6","V12","V14","V15"),
            c("Event Number","Days to TCA","Pc Best","Pc Nom","ProbCatIfColl",
