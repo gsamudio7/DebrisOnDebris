ae45c973b394a7845407b085a03305071e76f814
developer <developer@developers-MBP.fios-router.home>
2021-07-20 20:10:36 -0400

    deleted MATLAB data

--- initial_eda.R
+++ initial_eda.R
@@ -4,18 +4,18 @@ library(data.table)
 library(dplyr)
 
 
-## Read in the data (executed before initial clean)
+# Read in the data (executed before initial clean)
 observations <- fread("DebrisOnDebris.csv", fill=TRUE)
 
 
-# # Transform data to only the relevant columns (based on Mr. Hejduk's guidance)
+# Transform data to only the relevant columns (based on Mr. Hejduk's guidance)
 observations <- observations[,c(48,49,9,6,12,14,15)]
 observations <- observations %>%
   setnames(c("V48","V49","V9","V6","V12","V14","V15"),
            c("Event Number","Days to TCA","Pc Best","Pc Nom","ProbCatIfColl",
              "NumFragIfCatColl","NumFragIfNonCatColl"))
-# 
-# # Write
+
+# Write
 fwrite(observations,"trimmedData.csv")
 
 
