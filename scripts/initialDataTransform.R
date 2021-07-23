ae93d7226b678c2a9ae65d1de3663c4f72eda49e
developer <gsamudio7@gmail.com>
2021-07-23 09:30:36 -0400

    pushing new new events.csv

--- scripts/initialDataTransform.R
+++ scripts/initialDataTransform.R
@@ -2,23 +2,25 @@
 # ###################
 # # Process Data ####
 # ###################
-# 
-# 
+
 # Imports
+rm(list=ls())
 library(data.table)
 library(dplyr)
 library(fst)
+library(readxl)
 
 ########################################
 # This part completed - do not re run 
-# DebrisOnDebris3.txt not in repo 
+# DebrisOnDebris3.xlsx not in repo 
 
-# # Read in the raw data
-# debrisData <- fread("data/DebrisOnDebris3.txt")
+# Read in the raw data
+# debrisData <- read_excel("data/DebrisOnDebris3.xlsx")
 # dim(debrisData) # 707188 rows and 58 columns
-# 
-# # Write and compress to store in Git repo
+
+# Write and compress to store in Git repo and delete original .xlsx file
 # write_fst(debrisData, "data/DebrisOnDebris3.fst", compress = 100)
+# unlink("data/DebrisOnDebris3.xlsx")
 
 # Now entire data set is compressed and can be stored in Git repo
 ###########################################
@@ -28,7 +30,8 @@ library(fst)
 #############################################
 
 # Read in .fst file and convert to data.table for fast compute
-events <- read_fst("data/DebrisOnDebris3.fst") %>% data.table()
+read_fst("data/DebrisOnDebris3.fst") %>% data.table() %>% fwrite("data/debrisData.csv")
+events <- fread("data/debrisData.csv",header=FALSE)
 dim(events) # 707188/58 -> no loss of data
 
 # Transform data to only the relevant columns (based on Mr. Hejduk's guidance)
@@ -51,6 +54,11 @@ events[,"time2TCA" := TCA - time_of_screening]
 
 # Write and push to Git
 fwrite(events,"data/events.csv")
+
+# Remove redundant data in data file
+unlink(c("data/DebrisOnDebris_v2.fst",
+         "data/DebrisOnDebris.fst",
+         "data/debrisData.csv"))
 
 # Git commands in the terminal:
 # git add .
