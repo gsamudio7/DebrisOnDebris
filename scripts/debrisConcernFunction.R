157a07549765f3636d0e0cbcfdd629cf7319ae04
developer <gsamudio7@gmail.com>
2021-07-28 14:44:42 -0400

    draft RMD

--- scripts/debrisConcernFunction.R
+++ scripts/debrisConcernFunction.R
@@ -8,60 +8,173 @@ library(RColorBrewer)
 events <- fread("data/events.csv")
 
 # Get the events of concern with Pc_concern = 1e-5
-Pc_concern <- 1e-5
+Pc_concernVector <- c(1e-3,1e-4,1e-5)
 
-concernSummary <- events[!is.na(PcBest),
-  .(bool=time2TCA==min(time2TCA) & time2TCA < 1,
-    PcBest=PcBest),
-  by=eventNumber][bool==TRUE,!"bool"] 
+# Summarize conjunction events of concern for each FragNum
+concernSummary <- rbindlist(list(
+  
+  # PcBest
+  events[!is.na(PcBest),
+    .(bool=time2TCA==min(time2TCA) & time2TCA < 1,
+      fragNum="PcBest",
+      Pc_min=PcBest),
+    by=eventNumber][bool==TRUE,!"bool"],
+  
+  # PcFrag10
+  events[!is.na(PcFrag10),
+   .(bool=time2TCA==min(time2TCA) & time2TCA < 1,
+     fragNum="PcFrag10",
+     Pc_min=PcFrag10),
+   by=eventNumber][bool==TRUE,!"bool"],
+  
+  # PcFrag100
+  events[!is.na(PcFrag100),
+   .(bool=time2TCA==min(time2TCA) & time2TCA < 1,
+     fragNum="PcFrag100",
+     Pc_min=PcFrag100),
+   by=eventNumber][bool==TRUE,!"bool"],
+  
+  # PcFrag1000
+  events[!is.na(PcFrag1000),
+   .(bool=time2TCA==min(time2TCA) & time2TCA < 1,
+     fragNum="PcFrag1000",
+     Pc_min=PcFrag1000),
+   by=eventNumber][bool==TRUE,!"bool"],
+  
+  # PcFrag10000
+  events[!is.na(PcFrag10000),
+   .(bool=time2TCA==min(time2TCA) & time2TCA < 1,
+     fragNum="PcFrag10000",
+     Pc_min=PcFrag10000),
+   by=eventNumber][bool==TRUE,!"bool"]
+))
+
+concernSummary[,"fragNum" := as.factor(fragNum)]
+concernSummary[,"Pc_min" := as.double(Pc_min)]
+
+# How many are 0?
+zeroCount <- merge(
+  concernSummary[Pc_min==0,.(Zero_count=.N),by=fragNum],
+  concernSummary[,.(Total=.N),by=fragNum],
+  by="fragNum")
+zeroCount[,"Proportion" := Zero_count/Total]
+zeroCount <- zeroCount[,!"Total"]
+zeroCount
+
+# How many concern events at varying frag numbers values?
+concernCount <- merge(
+  concernSummary[Pc_min >= 1e-5,.(Concern_Event_Count=.N),by=fragNum],
+  concernSummary[,.(Total=.N),by=fragNum]
+)
+concernCount[,"Proportion" := Concern_Event_Count/Total]
+concernCount # Frag 10000 has no concern events
+
+
+# Explore distribution of Collision probabilities, for different fragment numbers
+Pc_at_1_day <- concernSummary %>%
+  plot_ly(
+    type="histogram",
+    x=~log(Pc_min),
+    color=~fragNum,
+    nbinsx=35
+  ) %>%
+  
+  layout(title = '<b>Collision Probability Distribution\nwhen TCA < 1 day</b>',
+         xaxis = list(title="<b>Collision Probability</b>",
+                      tickvals = seq(-720,-20,100),
+                      ticktext = seq(-720,-20,100) %>% exp() %>% formatC(format="e",digits=2),
+                      tickfont = list(size = 10),
+                      tickangle = 90),
+         yaxis = list(title="<b>Frequency</b>"),
+         hovermode = "x unified",
+         
+         shapes = list(
+           list(type ="line",
+                line = list(color="black"),
+                x0 = log(1e-5), x1 = log(1e-5),
+                y0 = 0, y1 = 2400),
+           list(type ="line",
+                line = list(color="black"),
+                x0 = log(1e-100), x1 = log(1e-100),
+                y0 = 0, y1 = 2150),
+           list(type ="line",
+                line = list(color="black"),
+                x0 = log(1e-200), x1 = log(1e-200),
+                y0 = 0, y1 = 1900)),
+           
+         annotations = list(
+           list(text = paste("<b>1e-5</b>"),
+                x = log(1e-5), y = 2500, showarrow=FALSE),
+           list(text = paste("<b>1e-100</b>"),
+                x = log(1e-100), y = 2250, showarrow=FALSE),
+           list(text = paste("<b>1e-200</b>"),
+                x = log(1e-200), y = 2000, showarrow=FALSE))
+  )
+  
+
+save(Pc_at_1_day,file="products/Pc_at_1_day.RData")
+
+# How does the Pc change as we approach TCA
+events[,"TCA_Bin" := as.factor(ceiling(time2TCA))]
 
 # Include the PcBest values at given days to TCA
-concernSummary_at_TOI <- events[!is.na(PcBest) &
-  eventNumber %in% concernSummary$eventNumber & 
-  time2TCA <= days_to_TCA & time2TCA > days_to_TCA - 1,
-  .(PcBest_at_5=PcBest,
+concernSummary_at_TOI <- events[!is.na(PcBest),
+  .(PcBest_at_TOI=PcBest,
     bool=time2TCA==max(time2TCA)),
-  by=eventNumber][bool==TRUE,!"bool"] %>%
+  by=c("TCA_Bin","eventNumber")][bool==TRUE,!"bool"] %>%
 
-  merge.data.table(concernSummary,by="eventNumber")
+  merge.data.table(concernSummary[fragNum=="PcBest"],by="eventNumber")
 
+# How many zeros?
+zeroCount <- merge(
+  concernSummary_at_TOI[PcBest_at_TOI==0,.(Zero_count=.N),by=TCA_Bin],
+  concernSummary_at_TOI[,.(Total=.N),by=TCA_Bin],
+  by="TCA_Bin")
+zeroCount[,"Proportion" := Zero_count/Total]
+zeroCount <- zeroCount[,!"Total"]
+zeroCount
+
+# Plot
+Pc_by_TCA_plot <- concernSummary_at_TOI %>%
+  plot_ly(
+    type="box",
+    x=~TCA_Bin,
+    y=~log(PcBest_at_TOI)
+  ) %>%
+  layout(
+    yaxis = list(title="<b>Probability of Collision</b>",
+                 tickvals = seq(-700,0,100),
+                 ticktext = seq(-700,0,100) %>% exp() %>% formatC(format="e",digits=2),
+                 tickfont = list(size = 10))
+  )
+  
+save(Pc_by_TCA_plot,file="products/Pc_by_TCA_plot.RData")
+  
 
 # Count total positives (events of concern) that we have visibility on at 5 days out
-P_at_5 <- concernSummary_at_5[PcBest >= Pc_concern,.N] # 22
-N_at_5 <- concernSummary_at_5[PcBest < Pc_concern,.N] # 29322
+Pc_concern <- 1e-5
+(P_at_5 <- concernSummary_at_TOI[TCA_Bin==5 & PcBest_at_TOI >= Pc_concern,.N]) # 21
+(N_at_5 <- concernSummary_at_TOI[TCA_Bin==5 & PcBest_at_TOI < Pc_concern,.N]) # 29323
 
-P <- concernSummary[PcBest >= Pc_concern,.N] # 58
-N <- concernSummary[PcBest < Pc_concern,.N] # 48193
-
-# Count false negatives (PcBest_at_5 < Pc_warn and PcBest >= Pc_concern) if we use naive approach (risk averse)
-Pc_warn <- concernSummary_at_5[PcBest >= Pc_concern,min(PcBest_at_5)]
+(P <- concernSummary[fragNum=="PcBest" & Pc_min >= Pc_concern,.N]) # 58
+(N <- concernSummary[fragNum=="PcBest" & Pc_min < Pc_concern,.N]) # 48193
 
 # Produce confusion matrix
 debrisConfusion <- function(Pc_warn,days_to_TCA,Pc_concern=1e-5,verbose=FALSE) {
   
-  # Include the PcBest values at given days to TCA
-  concernSummary_at_TOI <- 
-    events[!is.na(PcBest) & 
-    eventNumber %in% concernSummary$eventNumber & 
-    time2TCA <= days_to_TCA & 
-    time2TCA > days_to_TCA - 1,
-      .(PcBest_at_5=PcBest,
-        bool=time2TCA==max(time2TCA)),
-          by=eventNumber][bool==TRUE,!"bool"] %>%
-    
-    merge.data.table(concernSummary,by="eventNumber")
+  df <- concernSummary_at_TOI[TCA_Bin==days_to_TCA]
   
-  TPR <- concernSummary_at_TOI[PcBest >= Pc_concern & PcBest_at_5 >= Pc_warn,.N]/P
-  FPR <- concernSummary_at_TOI[PcBest < Pc_concern & PcBest_at_5 >= Pc_warn,.N]/N
-  TNR <- concernSummary_at_TOI[PcBest < Pc_concern & PcBest_at_5 < Pc_warn,.N]/N
-  FNR <- concernSummary_at_TOI[PcBest >= Pc_concern & PcBest_at_5 < Pc_warn,.N]/P
+  TPR <- df[PcBest_at_TOI >= Pc_warn & Pc_min >= Pc_concern,.N]/P
+  FPR <- df[PcBest_at_TOI >= Pc_warn & Pc_min < Pc_concern,.N]/N
+  TNR <- df[PcBest_at_TOI < Pc_warn & Pc_min < Pc_concern,.N]/N
+  FNR <- df[PcBest_at_TOI < Pc_warn & Pc_min >= Pc_concern,.N]/P
   if (verbose==TRUE) {
     cat(sprintf(
       "Pc_warn: %g\nPc_concern: %g\n\nTPR: %g | FPR: %g \nTNR: %g | FNR: %g",
       Pc_warn,Pc_concern,TPR,FPR,TNR,FNR)
     )
   }
-  return(data.table("Warn_Threshold"=c(Pc_warn,Pc_warn),
+  return(data.table("Warn_Threshold"=c(Pc_warn,Pc_warn,Pc_warn,Pc_warn),
                     "Rate"=c(FNR,FPR,TPR,TNR),
                     "Rate Type"=c("False Negative Rate",
                                   "False Positive Rate",
@@ -70,15 +183,8 @@ debrisConfusion <- function(Pc_warn,days_to_TCA,Pc_concern=1e-5,verbose=FALSE) {
   )
 }
 
-# Use min (risk averse)
-debrisConfusion(Pc_warn=Pc_warn,
-                days_to_TCA=2)
-
-# Use max (risk tolerant)
-debrisConfusion(Pc_warn=concernSummary_at_5[PcBest >= Pc_concern,max(PcBest_at_5)])
-
 # Get data for a range of values
-rates <- lapply(
+concernRates <- lapply(
   seq(from=1e-9,
       to=1e-4,
       by=1e-6),
@@ -101,7 +207,7 @@ rates <- lapply(
   )
 
 
-
+save(concernRates,file="products/concernRates.RData")
 
 
 
